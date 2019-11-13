869dba8354f7fe24f59c0ac255c0d3f82459dc42
Keith Brewster <keith.brewster@cbc.ca>
2019-11-12 22:24:29 -0500

    feat(app): set up main app structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-const React = require('react')
-const PropTypes = require('prop-types')
-const { Text } = require('ink')
+const React = require("react")
+const PropTypes = require("prop-types")
+const { useInput, useApp, Static } = require("ink")
+const { useState } = React
+
+const ScriptList = require("./ScriptList")
+const QUIT = "q"
 
 const App = ({ scripts }) => {
-  return <Text>Test</Text>
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const { exit } = useApp()
+
+  const handleInput = useInput((input, key) => {
+    let idx = currentIndex
+    if (input === QUIT) {
+      exit()
+    }
+    if (key.upArrow) {
+      if (currentIndex > 0) {
+        setCurrentIndex(--idx)
+      }
+    }
+    if (key.downArrow) {
+      if (currentIndex < Object.keys(scripts).length) {
+        setCurrentIndex(++idx)
+      }
+    }
+    if (key.return) {
+      // handle select logic
+    }
+  })
+
+  return (
+    <div onKeyDown={handleInput}>
+      <Static>SCRIPT STRIP</Static>
+      <ScriptList scripts={scripts} currentIndex={currentIndex} />
+    </div>
+  )
 }
 
 App.propTypes = {
-  scripts: PropTypes.object,
+  scripts: PropTypes.object.isRequired,
+}
+
+App.defaultProps = {
+  scripts: {},
 }
 
 module.exports = App
